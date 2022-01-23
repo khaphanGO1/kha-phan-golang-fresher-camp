71ed01985b47e4667be4a3392391f4c7c308b95f
khaphanGO1 <97881307+khaphanGO1@users.noreply.github.com>
2022-01-23 18:20:37 +0700

    Add files via upload

--- Untitled.sql
+++ Untitled.sql
@@ -14,7 +14,7 @@ CREATE TABLE `Foods` (
 CREATE TABLE `Restaurants` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255),
-  `adress` varchar(255),
+  `adress_id` int,
   `phone number` varchar(255),
   `rated` float,
   `created at` timestamp,
@@ -26,17 +26,18 @@ CREATE TABLE `Categories` (
   `name` varchar(255)
 );
 
-CREATE TABLE `user` (
+CREATE TABLE `users` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `full_name` varchar(255),
   `email` varchar(255),
+  `adress_id` int,
   `phone number` varchar(255),
   `created at` timestamp,
   `update at` timestamp
 );
 
-CREATE TABLE `order` (
-  `id` int PRIMARY KEY,
+CREATE TABLE `orders` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
   `user_id` int UNIQUE NOT NULL,
   `status` varchar(255),
   `created at` timestamp,
@@ -56,17 +57,49 @@ CREATE TABLE `Restaurants_Categories` (
   `update at` timestamp
 );
 
+CREATE TABLE `Adresses` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `full_name` varchar(255),
+  `phone number` varchar(255),
+  `state` varchar(255),
+  `city` varchar(255),
+  `street` varchar(255),
+  `adress` varchar(255)
+);
+
+CREATE TABLE `Favorites_Restaurants` (
+  `user_id` int,
+  `restaurant_id` int
+);
+
+CREATE TABLE `Favorites_Foods` (
+  `user_id` int,
+  `food_id` int
+);
+
 ALTER TABLE `Restaurants` ADD FOREIGN KEY (`id`) REFERENCES `Foods` (`restaurant_id`);
 
-ALTER TABLE `order` ADD FOREIGN KEY (`id`) REFERENCES `order_foods` (`order_id`);
+ALTER TABLE `orders` ADD FOREIGN KEY (`id`) REFERENCES `order_foods` (`order_id`);
 
 ALTER TABLE `Foods` ADD FOREIGN KEY (`id`) REFERENCES `order_foods` (`food_id`);
 
-ALTER TABLE `user` ADD FOREIGN KEY (`id`) REFERENCES `order` (`user_id`);
+ALTER TABLE `users` ADD FOREIGN KEY (`id`) REFERENCES `orders` (`user_id`);
 
 ALTER TABLE `Foods` ADD FOREIGN KEY (`categorie_id`) REFERENCES `Categories` (`id`);
 
 ALTER TABLE `Restaurants` ADD FOREIGN KEY (`id`) REFERENCES `Restaurants_Categories` (`restaurant_id`);
 
 ALTER TABLE `Categories` ADD FOREIGN KEY (`id`) REFERENCES `Restaurants_Categories` (`categorie_id`);
+
+ALTER TABLE `Adresses` ADD FOREIGN KEY (`id`) REFERENCES `Restaurants` (`adress_id`);
+
+ALTER TABLE `Adresses` ADD FOREIGN KEY (`id`) REFERENCES `users` (`adress_id`);
+
+ALTER TABLE `users` ADD FOREIGN KEY (`id`) REFERENCES `Favorites_Restaurants` (`user_id`);
+
+ALTER TABLE `users` ADD FOREIGN KEY (`id`) REFERENCES `Favorites_Foods` (`user_id`);
+
+ALTER TABLE `Restaurants` ADD FOREIGN KEY (`id`) REFERENCES `Favorites_Restaurants` (`restaurant_id`);
+
+ALTER TABLE `Foods` ADD FOREIGN KEY (`id`) REFERENCES `Favorites_Foods` (`food_id`);
 
