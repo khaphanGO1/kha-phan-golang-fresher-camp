922254bde21d4fad9601e013857c7a7410c5ea8a
khaphanGO1 <97881307+khaphanGO1@users.noreply.github.com>
2022-02-07 12:02:13 +0700

    Add files via upload

---
+++ modules/restaurant/restauranttransport/ginrestaurant/get_restaurant.go
@@ -0,0 +1,32 @@
+package ginrestaurant
+
+import (
+	"demo/common"
+	"demo/component"
+	"demo/modules/restaurant/restaurantbiz"
+	"demo/modules/restaurant/restaurantstorage"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+func GetRestaurant(appCtx component.AppContext) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			panic(common.ErrInvalidRequest(err))
+		}
+
+		store := restaurantstorage.NewSQLStore(appCtx.GetMainDBConnection())
+		biz := restaurantbiz.NewGetRestaurantBiz(store)
+
+		data, err := biz.GetRestaurant(c.Request.Context(), id)
+
+		if err != nil {
+			panic(err)
+		}
+
+		c.JSON(http.StatusOK, common.SimpleSuccessResponse(data))
+	}
+}
---
+++ modules/restaurant/restauranttransport/ginrestaurant/list_restaurant.go
@@ -0,0 +1,40 @@
+package ginrestaurant
+
+import (
+	"demo/common"
+	"demo/component"
+	"demo/modules/restaurant/restaurantbiz"
+	"demo/modules/restaurant/restaurantmodel"
+	"demo/modules/restaurant/restaurantstorage"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func ListRestaurant(appCtx component.AppContext) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var filter restaurantmodel.Filter
+
+		if err := c.ShouldBind(&filter); err != nil {
+			panic(common.ErrInvalidRequest(err))
+		}
+
+		var paging common.Paging
+
+		if err := c.ShouldBind(&paging); err != nil {
+			panic(common.ErrInvalidRequest(err))
+		}
+
+		paging.Fulfill()
+
+		store := restaurantstorage.NewSQLStore(appCtx.GetMainDBConnection())
+		biz := restaurantbiz.NewListRestaurantBiz(store)
+
+		result, err := biz.ListRestaurant(c.Request.Context(), &filter, &paging)
+
+		if err != nil {
+			panic(err)
+		}
+
+		c.JSON(http.StatusOK, common.NewSuccessResponse(result, paging, filter))
+	}
+}
